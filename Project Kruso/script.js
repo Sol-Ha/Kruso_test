d456e9d2843b13a8bb255366a0a6531964b623e5
Sol-ha <101246194+Sol-Ha@users.noreply.github.com>
2022-04-17 22:57:19 +0200

    Delete Project Kruso directory

--- Project Kruso/script.js
+++
@@ -1,7 +0,0 @@
-const scrollDown = document.querySelectorAll("li");
-
-for(i=0; i<scrollDown.length; i++){
-    scrollDown[i].addEventListener("click", function(){
-        this.classList.toggle("active")
-    })
-}
