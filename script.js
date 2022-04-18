47729b7ca734733471eeafcd82fb8fc1502dd9e4
Sol-ha <101246194+Sol-Ha@users.noreply.github.com>
2022-04-18 22:51:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const scrollDown = document.querySelectorAll("li");
-
+// variable
+const scrollDown = document.getElementsByTagName("li");
+// event by click to toggle to switch between open and close
 for(i=0; i<scrollDown.length; i++){
     scrollDown[i].addEventListener("click", function(){
-        this.classList.toggle("active")
+        this.classList.toggle("active");
     })
 }
+
